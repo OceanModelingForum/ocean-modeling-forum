b43d548b24abcb76a49adaa695a07968fa638e34
Darin Reid <darinreid@gmail.com>
2015-05-19 10:42:57 -0700

    Adding apple-touch-icon

--- wp-content/themes/omf/templates/header/head.php
+++ wp-content/themes/omf/templates/header/head.php
@@ -3,6 +3,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() . '/assets/dst/images/apple-touch-icon.png'; ?>">
+
     <?php wp_head(); ?>
 
     <script src="//use.typekit.net/ycr5cje.js"></script>
