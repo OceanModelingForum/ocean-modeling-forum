59173aaeb3113bbe75fdd54cc41d833d3e344882
Darin Reid <darinreid@gmail.com>
2015-06-01 08:19:44 -0700

    Setting up new deploy script and config

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,65 +1,118 @@
-<?php
-if (! file_exists(dirname(__FILE__) . '/env.php')) {
-	throw new Exception('Could not find an env.php file with local configuration settings.');
-}
 
-$env = require(dirname(__FILE__) . '/env.php');
+@setup
 
-$production = $env['production'];
-?>
+    /**
+     * Define deployment environments
+     *
+     * Default is the first setting in this array.
+     */
+    $environments = [
+        'deploy' => include('envoy-config.php'),
+    ];
 
-@servers(['production' => $production['ssh']]);
+    /**
+     * Set options based on environment
+     */
+    $opts = isset($env) ? $environments[$env] : reset($environments);
 
-<?php
+    /**
+     * These settings are required and must not be empty:
+     */
+    $required_not_empty = ['ssh', 'repo', 'releases_dir', 'app_symlink', 'shared_dir', 'release_limit'];
 
-$repo           = $production['repository'];
-$release_dir    = $production['release_dir'];
-$app_dir        = $production['app_dir'];
-$shared_dir     = $production['shared_dir'];
-$shared_assets  = $production['shared_assets'];
-$composer_dir   = $production['composer_dir'];
-$composer_executable = $production['composer_executable'];
-$release = date('YmdHis');
-?>
+    /**
+     * These settings are required, but may be empty (''):
+     */
+    $required = ['scripts_dir', 'composer_exec', 'shared'];
 
-@macro('deploy', ['on' => 'production'])
-    fetch_repo
+    /**
+     * Check for required settings
+     */
+    foreach ($required_not_empty as $setting) if ( ! isset($opts[$setting]) || empty($opts[$setting])) throw new Execption($setting . ' must be set and not empty.');
+    foreach ($required as $setting) if ( ! isset($opts[$setting])) throw new Execption($setting . ' must be set.');
+
+    /**
+     * Extract options
+     */
+    extract($opts);
+
+    /**
+     * Release name is current datetime
+     */
+    $release = date('YmdHis');
+
+    /**
+     * Define the Git branch to use
+     */
+    $branch = isset($branch) ? $branch : 'master';
+
+@endsetup
+
+@servers(['deploy' => $opts['ssh']])
+
+@macro('deploy')
+    clone_repository
     run_composer
     update_permissions
-    update_app_symlink
+    symlink_new_release
     symlink_shared_assets
+    remove_old_releases
 @endmacro
 
-@macro('rollback', ['on' => 'production'])
+@task('rollback')
+    cd {{ $releases_dir }};
 
-@endmacro
+    # Symlink previous release
+    find . -maxdepth 1 -type d -name '2*' | sort -r | head -n2 | tail -n1 | cut -c 3- | xargs -I '{}' ln -nfs {{ $releases_dir }}/'{}' {{ $app_symlink }};
 
-@task('fetch_repo')
-    [ -d {{ $release_dir }} ] || mkdir {{ $release_dir }};
-    cd {{ $release_dir }};
-    git clone -b master --depth=1 {{ $repo }} {{ $release }};
+    # Remove newest release
+    ls -1d * | sort -r | head -n1 | xargs -d '\n' rm -Rf;
+
+    echo "Rolled back to previous release";
+@endtask
+
+@task('clone_repository')
+    [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }};
+    cd {{ $releases_dir }};
+    git clone {{ $repo }} --branch={{ $branch }} --depth=1 {{ $release }};
+    echo 'Repository cloned';
 @endtask
 
 @task('run_composer')
-    cd {{ $release_dir }}/{{ $release }}/{{ $composer_dir }};
-    {{ $composer_executable }} install --prefer-dist;
+    @if ( ! empty($composer_exec))
+        cd {{ $releases_dir }}/{{ $release }}/{{ $scripts_dir }};
+        {{ $composer_exec }} install --prefer-dist --no-interaction --no-dev;
+    @else
+        echo '';
+    @endif
 @endtask
 
 @task('update_permissions')
-    cd {{ $release_dir }};
+    cd {{ $releases_dir }};
     {{-- chgrp -R www-data {{ $release }}; --}}
     chmod -R ug+rwx {{ $release }};
 @endtask
 
+@task('symlink_new_release')
+    ln -nfs {{ $releases_dir }}/{{ $release }} {{ $app_symlink }};
+    {{-- chgrp -h www-data {{ $app_symlink }}; --}}
+@endtask
+
 @task('symlink_shared_assets')
-    @foreach($shared_assets as $asset)
-        rm -rf {{ $release_dir }}/{{ $release }}/{{ $asset }};
-        ln -nfs {{ $shared_dir }}/{{ $asset }} {{ $release_dir }}/{{ $release }}/{{ $asset }};
-        {{-- chgrp -h www-data {{ $release_dir }}/{{ $release }}/{{ $asset }}; --}}
+    [ -d {{ $shared_dir }} ] || mkdir {{ $shared_dir }};
+    @foreach($shared as $asset)
+        rm -rf {{ $releases_dir }}/{{ $release }}/{{ $asset }};
+        ln -nfs {{ $shared_dir }}/{{ $asset }} {{ $releases_dir }}/{{ $release }}/{{ $asset }};
+        {{-- chgrp -h www-data {{ $releases_dir }}/{{ $release }}/{{ $asset }}; --}}
     @endforeach
 @endtask
 
-@task('update_app_symlink')
-    ln -nfs {{ $release_dir }}/{{ $release }} {{ $app_dir }};
-    {{-- chgrp -h www-data {{ $app_dir }}; --}}
+@task('update_wp_super_cache')
+    echo '';
+@endtask
+
+@task('remove_old_releases')
+    cd {{ $releases_dir }};
+    find . -maxdepth 1 -type d -name '2*' | sort | head -n -{{ $release_limit }} | xargs -d '\n' rm -Rf;
+    echo 'Cleaned up old deployments';
 @endtask
